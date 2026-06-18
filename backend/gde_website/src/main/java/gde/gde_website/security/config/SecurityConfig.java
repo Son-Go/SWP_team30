f6f690139740c5270b389d5715c99e26abcb0f6f
Son-Go <gorelovartemij@gmail.com>
2026-06-18 14:14:43 +0300

    feature: edit security config

--- backend/gde_website/src/main/java/gde/gde_website/security/config/SecurityConfig.java
+++ backend/gde_website/src/main/java/gde/gde_website/security/config/SecurityConfig.java
@@ -1,26 +1,36 @@
 package gde.gde_website.security.config;
 
+import gde.gde_website.security.JwtFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtFilter jwtFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
                         // Allow requests for /games endpoints without authentication
-                        .requestMatchers(org.springframework.http.HttpMethod.GET, "/games").permitAll()
-                        .requestMatchers(org.springframework.http.HttpMethod.GET, "/games/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/games").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/games/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/games/**").authenticated()
                         // #TODO: debug mode
                         .anyRequest().permitAll()
                 )
+                .sessionManagement(s -> s
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 // Disable Basic Auth for REST API
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
