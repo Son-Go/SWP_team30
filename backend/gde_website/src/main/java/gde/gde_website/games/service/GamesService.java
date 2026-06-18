a4341dce5295af04bee8cc90cd84d4fca6a6596c
Son-Go <gorelovartemij@gmail.com>
2026-06-18 16:32:11 +0300

    feature: add delete game endpoint

--- backend/gde_website/src/main/java/gde/gde_website/games/service/GamesService.java
+++ backend/gde_website/src/main/java/gde/gde_website/games/service/GamesService.java
@@ -1,10 +1,13 @@
 package gde.gde_website.games.service;
 
+import gde.gde_website.games.controller.GamesController;
 import gde.gde_website.games.entity.GamesEntity;
 import gde.gde_website.games.mapper.GamesMapper;
 import gde.gde_website.games.model.Games;
 import gde.gde_website.games.model.GamesResponce;
 import gde.gde_website.games.repository.GamesRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
@@ -13,6 +16,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class GamesService {
+    private static final Logger gamesServiceLogger = LoggerFactory.getLogger(GamesService.class);
 
     private final GamesRepository repository;
     private final GamesMapper mapper;
@@ -23,10 +27,12 @@ public class GamesService {
     }
 
     public Page<GamesEntity> getAllGames(Pageable pageable) {
+        gamesServiceLogger.info("Called getAllGames method");
         return repository.findAllByOrderByCreatedAtDesc(pageable);
     }
 
     public GamesResponce getGameById(Long gameId, Long currentUserId) {
+        gamesServiceLogger.info("Called getGameById method");
         GamesEntity game = repository.findById(gameId).
                 orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 
@@ -34,6 +40,7 @@ public class GamesService {
     }
 
     public Games createGame(Games entity, Long authorId) {
+        gamesServiceLogger.info("Called createGame method");
         GamesEntity game = new GamesEntity(
                 authorId,
                 entity.title(),
@@ -42,6 +49,22 @@ public class GamesService {
         );
 
         GamesEntity savedGame = repository.save(game);
+        gamesServiceLogger.info("Successfully created game");
         return mapper.entityToGames(savedGame);
+    }
+
+    public Games deleteGame(Long id, Long currentUserId) {
+        gamesServiceLogger.info("Called deleteGame method");
+        GamesEntity entity = repository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Game not found"));
+
+        if (!entity.getAuthorId().equals(currentUserId)) {
+            gamesServiceLogger.error("User permissions error");
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "You are not the owner of this game");
+        }
+
+        repository.delete(entity);
+        gamesServiceLogger.info("Successfully deleted game");
+        return mapper.entityToGames(entity);
     }
 }
