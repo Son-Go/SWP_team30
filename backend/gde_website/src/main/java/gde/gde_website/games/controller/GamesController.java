986876f39917d0afd9581e94c66a33f71666d7bc
Egor <grishinegor44@gmail.com>
2026-06-27 13:39:08 +0300

    Clear code

--- backend/gde_website/src/main/java/gde/gde_website/games/controller/GamesController.java
+++ backend/gde_website/src/main/java/gde/gde_website/games/controller/GamesController.java
@@ -8,7 +8,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
