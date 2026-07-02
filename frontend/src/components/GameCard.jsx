9c98cd54bf51d858654efe7ead5141f2f65b164f
Zhend0sss <savel.zhenya@gmail.com>
2026-07-02 14:48:39 +0300

    feat: adding the authors ava to the GameCard + CSS improvements

--- frontend/src/components/GameCard.jsx
+++ frontend/src/components/GameCard.jsx
@@ -5,16 +5,36 @@ function GameCard({ game }) {
   return (
     <Link to={`/games/${game.id}`} className="card card-link">
       {game.bannerUrl ? (
-        <img src={game.bannerUrl} alt={game.title} />
+        <img src={game.bannerUrl} alt={game.title} className="card-banner" />
       ) : (
-        <div className="state-box">Баннер пока не загружен.</div>
+        <div className="state-box card-banner">Баннер пока не загружен.</div>
       )}
 
       <div className="section">
-        <h2 className="card-title">{game.title}</h2>
-        {game.author?.username && (
-          <p className="card-author">{game.author.username}</p>
-        )}
+        <div className="card-author-row">
+          {game.author?.username && (
+            <>
+              {game.author.profile_image_url ? (
+                <img
+                  src={game.author.profile_image_url}
+                  alt={game.author.username}
+                  className="card-author-avatar"
+                />
+              ) : (
+                <div className="card-author-avatar card-author-avatar-placeholder">
+                  {game.author.username[0].toUpperCase()}
+                </div>
+              )}
+              <div className="card-author-info">
+                <span className="card-author">{game.author.username}</span>
+                <h2 className="card-title">{game.title}</h2>
+              </div>
+            </>
+          )}
+          {!game.author?.username && (
+            <h2 className="card-title">{game.title}</h2>
+          )}
+        </div>
 
         {game.tags?.length > 0 && (
           <div className="tag-list">
