275bdeeaef7f64dd8f25d6c348677b11f12190f7
Egor <grishinegor44@gmail.com>
2026-07-10 11:35:02 +0300

    Refactor GamesCommentResponse; add new fields

--- backend/gde_website/src/main/java/gde/gde_website/games/model/GamesCommentResponse.java
+++ backend/gde_website/src/main/java/gde/gde_website/games/model/GamesCommentResponse.java
@@ -1,8 +1,12 @@
 package gde.gde_website.games.model;
 
+import java.time.Instant;
+
 public record GamesCommentResponse(
         Long id,
         AuthorResponse author,
-        String text
+        String text,
+        Instant createdAt,
+        Instant updatedAt
 ) {
 }
